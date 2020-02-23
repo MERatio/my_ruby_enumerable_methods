9a8bf1707f22289b4d0fbce08e380addd7f2ec9e
Mark Edward Ratio <markedwardratio@gmail.com>
2020-02-23 15:11:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+My implementation of Enumerable module methods in Ruby
---
+++ my_ruby_enumerable_methods.rb
@@ -0,0 +1,176 @@
+module Enumerable
+  def my_each
+    type = self.class.name
+    if (type == "Array" || type == "Range")
+      arr = self.to_a
+      for i in (0..arr.length - 1)
+        yield(arr[i])
+      end
+    elsif (type == "Hash")
+      keys = self.keys
+      values = self.values
+      for i in (0..self.length - 1)
+        yield(keys[i], values[i])
+      end
+    else
+      puts "Error: incorrect data type"
+    end
+  end
+
+  def my_each_with_index
+    type = self.class.name
+    if (type == "Array" || type == "Hash" || type == "Range")
+      arr = self.to_a
+      for i in (0..arr.length - 1)
+        yield(arr[i], i)
+      end
+    else
+      puts "Error: incorrect data type"
+    end
+  end
+
+  def my_select
+    type = self.class.name
+    if (type == "Array" || type == "Range")
+      arr = self.to_a
+      output = []
+      arr.my_each do |item|
+        output.push(item) if yield(item)
+      end
+      output
+    elsif (type == "Hash")
+      output = {}
+      arr.my_each do |key, value|
+        output[key] = value if (yield(key, value))
+      end
+      output
+    else
+      puts "Error: incorrect data type"
+    end
+  end
+
+  def my_all?
+    type = self.class.name
+    if (type == "Array" || type == "Range")
+      arr = self.to_a
+      arr.my_each do |item|
+        return false unless yield(item)
+      end
+      return true
+    elsif (type == "Hash")
+      keys = self.keys
+      values = self.values
+      for i in (0..self.length - 1)
+        return false unless yield(keys[i], values[i])
+      end
+      return
+    else
+      puts "Error: incorrect data type"
+    end
+  end
+
+  def my_any?
+    type = self.class.name
+    if (type == "Array" || type == "Range")
+      arr = self.to_a
+      arr.my_each do |item|
+        return true if yield(item)
+      end
+      return false
+    elsif (type == "Hash")
+      keys = self.keys
+      values = self.values
+      for i in (0..self.length - 1)
+        return true if yield(keys[i], values[i])
+      end
+      return false
+    else
+      puts "Error: incorrect data type"
+    end
+  end
+
+  def my_none?
+    type = self.class.name
+    if (type == "Array" || type == "Range")
+      arr = self.to_a
+      arr.my_each do |item|
+        return false if yield(item)
+      end
+      return true
+    elsif (type == "Hash")
+      keys = self.keys
+      values = self.values
+      for i in (0..self.length - 1)
+        return false if yield(keys[i], values[i])
+      end
+      return true
+    else
+      puts "Error: incorrect data type"
+    end
+  end
+
+  def my_count(arg = nil)
+    type = self.class.name
+    counter = 0
+    if (type == "Array" || type == "Range")
+      arr = self.to_a
+      return arr.length if !block_given? && arg == nil
+      arr.my_each do |item|
+        if (block_given? && yield(item) || item == arg)
+          counter += 1
+        end
+      end
+    else
+      puts "Error: incorrect data type"
+    end
+    counter
+  end
+
+  def my_map
+    type = self.class.name
+    result = []
+    if (type == "Array" || type == "Range")
+      arr = self.to_a
+      arr.my_each do |item|
+        result.push(yield(item))
+      end
+    elsif (type == "Hash")
+      keys = self.keys
+      values = self.values
+      for i in (0..self.size - 1)
+        result.push(yield(keys[i], values[i]))
+      end
+    else
+      puts "Error: incorrect data type"
+    end
+    result
+  end
+
+  # Only accepts this = my_inject(initial) { |memo, obj| block } → obj
+  def my_inject(initial = nil)
+    type = self.class.name
+    if (type == "Array" || type == "Range")
+      arr = self.to_a
+      acc = initial || arr[0]
+      for i in (0..arr.length - 1)
+        initial ? acc = yield(acc, arr[i]) : acc = yield(acc, arr[i]) if i != 0 
+      end
+    else
+      puts "Error: incorrect data type"
+    end
+    acc
+  end
+end
+
+
+def multiply_els(arr) 
+  arr.my_inject do |acc, cur|
+    acc * cur
+  end
+end
+
+p multiply_els([2,4,5]) #=> 40
+
+double = Proc.new { |i| i*i }
+
+p (1..4).my_map(&double) #=> [1, 4, 9, 16]
